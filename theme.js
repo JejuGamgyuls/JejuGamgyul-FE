9195b6a9d2e76d8e767a876d256f6cff832b40fc
hyorish03 <hyorish2001@gmail.com>
2024-09-06 16:26:05 +0900

    Chore: 프로젝트 초기 설정

---
+++ theme.js
@@ -0,0 +1,36 @@
+const theme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    gray: {
+      10: '#0D0D0D',
+      9: '#333333',
+      8: '#404040',
+      7: '#595959',
+      6: '#737373',
+      5: '#999999',
+      4: '#B3B3B3',
+      3: '#CCCCCC',
+      2: '#F0F0F0',
+      1: '#FAFAFA',
+    },
+    yellowOrange: '#E37653',
+    orange: '#FD825B',
+    darkOrange: '#E5A14E',
+    blue: '#386DE8',
+    green: '#3CC344',
+    yellowGreen: '#87C700',
+  },
+  busRouteStatus: {
+    red: '#FB5852',
+    orange: '#FFCE00',
+    green: '#3CC344',
+  },
+  system: {
+    error: '#D32F2F',
+    success: '#4CAF50',
+    warning: '#F9A825',
+  },
+};
+
+export default theme;
