69914f90bff4081963835596ff8ba28900ca9ba7
hyorish03 <hyorish2001@gmail.com>
2024-09-06 17:47:50 +0900

    Chore: eslint-import 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintPluginImport from 'eslint-plugin-import'; // 플러그인 추가
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: eslintPluginImport,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -30,6 +32,20 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': ['warn'],
+      // import 순서 강제
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'], // 내장 모듈과 외부 모듈을 먼저
+            ['internal'], // 내부 모듈
+            ['parent', 'sibling', 'index'], // 상위/형제/인덱스 모듈 순서로
+          ],
+          'newlines-between': 'always', // 그룹 간 줄바꿈을 강제
+          alphabetize: { order: 'asc', caseInsensitive: true }, // 알파벳 순으로 정렬
+        },
+      ],
     },
   },
 ];
