edf6299ade5e2ff4330eda16464ede51c2e29d1f
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-11 16:19:36 +0900

    JG-19 Feat: Geolocation API를 적용한 현재 위치 반영

--- src/pages/mainpage/Map.jsx
+++ src/pages/mainpage/Map.jsx
@@ -1,10 +1,29 @@
 import { Marker, NaverMap, useNavermaps } from 'react-naver-maps';
 
+import useCurrentLocation from '../../hooks/useCurrentLocation.js';
+
+const geolocationOptions = {
+  enableHighAccuracy: true,
+  timeout: 1000 * 60 * 1, // 1 min (1000 ms * 60 sec * 1 minute = 60 000ms)
+  maximumAge: 1000 * 3600 * 24, // 24 hour
+};
 function Map() {
   const navermaps = useNavermaps();
+  const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
+
+  if (currentError) {
+    return <div>Error: {currentError.message}</div>;
+  }
+
+  if (!currentLocation) {
+    return <div>Loading...</div>;
+  }
+
+  const { latitude, longitude } = currentLocation;
+
   return (
-    <NaverMap defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)} defaultZoom={10}>
-      <Marker defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)} />
+    <NaverMap defaultCenter={new navermaps.LatLng(latitude, longitude)} defaultZoom={16}>
+      <Marker position={new navermaps.LatLng(latitude, longitude)} />
     </NaverMap>
   );
 }
--- src/pages/mainpage/index.jsx
+++ src/pages/mainpage/index.jsx
@@ -4,6 +4,7 @@ import NaverMapContainer from './NaverMapContainer';
 
 function Mainpage() {
   const naverMapClientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
+
   return (
     <>
       <NavermapsProvider ncpClientId={naverMapClientId}>
