0d7596ee52208ea7d4618168692261e47d1df8d4
hyorish03 <hyorish2001@gmail.com>
2024-09-12 15:36:43 +0900

    JG-18 Rebase with develop

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,12 @@
 import * as S from './styles';
+import UserIcon from '../../../public/svg/UserIcon.svg?react';
 
 function NavigationBar() {
-  return <S.NavigationBar>sdfsdfsdfs</S.NavigationBar>;
+  return (
+    <S.NavigationBar>
+      <S.NavIcon>{/* <UserIcon /> */}</S.NavIcon>
+    </S.NavigationBar>
+  );
 }
 
 export default NavigationBar;
--- src/components/NavigationBar/styles.js
+++ src/components/NavigationBar/styles.js
@@ -5,4 +5,17 @@ export const NavigationBar = styled.div`
   flex-direction: column;
   width: 63px;
   height: 100vh;
+  border-right: 1px solid rgba(6, 6, 6, 0.5);
+
+  padding: 0;
+  position: fixed;
+  z-index: 1;
+`;
+
+export const NavIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  &:hover {
+    background-color: #f5f5f5;
+  }
 `;
