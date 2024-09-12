43fd2c6b3d6e7ba00a734d19a3d707926f3d11c1
hyorish03 <hyorish2001@gmail.com>
2024-09-12 16:37:42 +0900

    JG-18 Chore: Resolve Conflict

--- src/pages/mainpage/index.jsx
+++ src/pages/mainpage/index.jsx
@@ -1,14 +1,11 @@
-import NavigationBar from '@components/NavigationBar/NavigationBar';
 import { NavermapsProvider } from 'react-naver-maps';
 
 import NaverMapContainer from './NaverMapContainer';
 
 function Mainpage() {
   const naverMapClientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
-
   return (
     <>
-      <NavigationBar />
       <NavermapsProvider ncpClientId={naverMapClientId}>
         <NaverMapContainer />
       </NavermapsProvider>
