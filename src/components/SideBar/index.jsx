5305842dd3a9e1bf1dc7e00628b57fe03681bd05
hyorish03 <hyorish2001@gmail.com>
2024-09-12 17:28:11 +0900

    JG-24 Feat: 사이드바 UI 개발 및 스타일링 수정

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,6 @@
+import * as S from './styles';
+function SideBar() {
+  return <S.Wrapper>sdf</S.Wrapper>;
+}
+
+export default SideBar;
--- src/pages/mainpage/index.jsx
+++ src/pages/mainpage/index.jsx
@@ -1,14 +1,18 @@
-import NavigationBar from '@components/NavigationBar/NavigationBar';
+import NavigationBar from '@components/NavigationBar';
+import SideBar from '@components/SideBar';
 import { NavermapsProvider } from 'react-naver-maps';
 
 import NaverMapContainer from './NaverMapContainer';
-
+import * as S from './styles';
 function Mainpage() {
   const naverMapClientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
 
   return (
     <>
-      <NavigationBar />
+      <S.SideWrapper>
+        <NavigationBar />
+        <SideBar />
+      </S.SideWrapper>
       <NavermapsProvider ncpClientId={naverMapClientId}>
         <NaverMapContainer />
       </NavermapsProvider>
