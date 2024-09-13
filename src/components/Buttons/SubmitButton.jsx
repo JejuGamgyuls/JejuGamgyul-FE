7a820bbea38793684bdfeb4c619ce2cffaa02200
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-13 17:47:38 +0900

    JG-27: 로그인 및 회원가입 위한 버튼 컴포넌트 구현

---
+++ src/components/Buttons/SubmitButton.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+function SubmitButton({ text }) {
+  return <StyledButton>{text}</StyledButton>;
+}
+const StyledButton = styled.button`
+  width: 360px;
+  height: 66px;
+  border-radius: 5px;
+  background: #fd825b;
+  border: none;
+  color: #fff;
+  font-family: Inter;
+  font-size: 19px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+export default SubmitButton;
