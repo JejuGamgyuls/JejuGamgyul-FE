aedcb5126c70d5af2067db3d867efc395ae4f1ae
hyorish03 <hyorish2001@gmail.com>
2024-09-13 14:05:04 +0900

    Feat: 사이드 바 컨텐츠 높이 상수화

--- src/components/SideBar/components/CurrentLocationHeader.jsx
+++ src/components/SideBar/components/CurrentLocationHeader.jsx
@@ -1,4 +1,5 @@
 import LocationIcon from '@assets/svg/LocationIcon.svg?react';
+import { STYLE } from '@constants/const';
 import useAddress from '@hooks/useAddress';
 import styled from 'styled-components';
 function CurrentLocationHeader() {
@@ -15,14 +16,14 @@ export default CurrentLocationHeader;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 50px;
+  height: ${STYLE.CURRENT_LOCATION_HEADER_HEIGHT}px;
   display: flex;
   align-items: center;
   font-size: 20px;
   font-weight: 700;
   color: #fff;
   margin-top: 15px;
-
+  border-bottom: 1px solid #e5e5e5;
   color: #000;
   font-family: Pretendard;
   font-size: 21px;
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,11 +1,22 @@
+import { STYLE } from '@constants/const';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 390px;
-  height: 100%;
+  height: 100vh;
   background-color: white;
   z-index: 2;
   display: flex;
   flex-direction: column;
   padding-top: 20px;
+  box-sizing: border-box;
+`;
+
+export const BusStopItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  overflow-y: scroll;
+  width: 100%;
+  height: calc(100vh - ${STYLE.BUS_STOP_ITEM_HEIGHT + STYLE.FIND_BUS_INPUT_HEIGHT}px);
 `;
