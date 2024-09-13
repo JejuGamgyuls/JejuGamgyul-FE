fcea96a3e44246623b076a711b98e7831dcd762f
hyorish03 <hyorish2001@gmail.com>
2024-09-13 17:46:59 +0900

    JG-28 Feat: 검색창 스타일링 수정

--- src/components/SideBar/components/FindBusInput.jsx
+++ src/components/SideBar/components/FindBusInput.jsx
@@ -1,10 +1,25 @@
 import FindIcon from '@assets/svg/SearchIcon.svg?react';
+import { navigationBarState } from '@atoms/navigationBarState';
+import { searchBusNumberState } from '@atoms/searchBusNumberState';
+import { CATEGORY } from '@constants/const';
+import { useRef } from 'react';
+import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 function FindBusInput() {
+  const inputRef = useRef();
+
+  const [setSearchBusNumber] = useRecoilState(searchBusNumberState);
+  const [setCategory] = useRecoilState(navigationBarState);
+
+  const handleSearchInfo = () => {
+    setSearchBusNumber(parseInt(inputRef.current.value));
+    setCategory(CATEGORY.BUSDETAILINFO);
+  };
+
   return (
     <Wrapper>
-      <StyledInput placeholder="버스 번호를 검색해보세요" />
-      <IconWrapper>
+      <StyledInput autoFocus ref={inputRef} placeholder="버스 번호를 검색해보세요" />
+      <IconWrapper onClick={handleSearchInfo}>
         <FindIcon width={25} height={25} />
       </IconWrapper>
     </Wrapper>
@@ -14,7 +29,7 @@ export default FindBusInput;
 
 const Wrapper = styled.div`
   width: 340px;
-  height: 45px;
+  height: 50px;
   border-radius: 5px;
   border: 2px solid #e37653;
   background: #fff;
@@ -22,19 +37,26 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 10px;
+  padding: 5px 10px;
   box-sizing: border-box;
 `;
 
 const StyledInput = styled.input`
+  width: 100%;
   height: 100%;
   border: none;
   background: transparent;
   font-size: 16px;
-
   &:focus {
     outline: none;
   }
 `;
 
-const IconWrapper = styled.div``;
+const IconWrapper = styled.div`
+  cursor: pointer;
+  width: 25px;
+  height: 100%;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+`;
