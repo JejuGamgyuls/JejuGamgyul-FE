38e1012b89dee64ca3075bff996b47719745be46
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-20 09:47:45 +0900

    JG-22 Refactor: 헤더 영역 레이아웃 수정

--- src/pages/busStopFindPage/components/BusStopHeader.jsx
+++ src/pages/busStopFindPage/components/BusStopHeader.jsx
@@ -5,37 +5,47 @@ import styled from 'styled-components';
 function BusStopHeader() {
   return (
     <Wrapper>
-      <BusStopInfo>
-        <BusStopName>&lt; 선릉역</BusStopName>
-        <IconWrapper>
-          <LocationIcon style={{ width: '24px', height: '24px' }} />
-          <FavoriteIcon style={{ width: '24px', height: '24px', cursor: 'pointer' }} />
-        </IconWrapper>
-      </BusStopInfo>
-      <BusArrivalInfo>
-        <ArrivalInfo>곧 도착</ArrivalInfo>
-        <BusNumber>333</BusNumber>
-      </BusArrivalInfo>
+      <InfoWrapper>
+        <BusStopInfo>
+          <BusStopName>&lt; 선릉역</BusStopName>
+          <IconWrapper>
+            <LocationIcon style={{ width: '24px', height: '24px' }} />
+            <FavoriteIcon style={{ width: '24px', height: '24px', cursor: 'pointer' }} />
+          </IconWrapper>
+        </BusStopInfo>
+        <BusArrivalInfo>
+          <ArrivalInfo>곧 도착</ArrivalInfo>
+          <BusNumber>333</BusNumber>
+        </BusArrivalInfo>
+      </InfoWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   width: 100%;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+`;
+const InfoWrapper = styled.div`
+  width: 332px;
   height: 130px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin: 26px 0px 0px 26px;
   color: #000;
   font-family: Pretendard;
 `;
 const BusStopInfo = styled.div`
-  width: 332px;
+  width: 100%;
   height: 56px;
-  margin: 26px 0 0 26px;
   display: flex;
   gap: 5px;
   font-size: 21px;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
+  padding-top: 26px;
 `;
 const BusStopName = styled.div`
   width: 88px;
@@ -49,11 +59,11 @@ const IconWrapper = styled.div`
   justify-content: space-between;
 `;
 const BusArrivalInfo = styled.div`
-  width: 332px;
+  width: 100%;
   height: 27px;
   border-radius: 3px;
   background: #f5f6f6;
-  margin: 6px 0 0 26px;
+  box-sizing: border-box;
   display: flex;
   align-items: center;
   gap: 10px;
