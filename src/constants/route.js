924402f1a971aa022bdeb0a35da2f132412ff658
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-20 13:21:34 +0900

    JG-29 Feat: 즐겨찾기 페이지 경로 설정

--- src/constants/route.js
+++ src/constants/route.js
@@ -2,6 +2,7 @@ export const ROUTE = {
   ROOT: '/',
   SIGNUP: '/signup',
   LOGIN: '/login',
+  FAVORITE: '/favorites',
   BUSFIND: '/search/bus/:busNumber',
   BUSSTOP: '/search/station/:busStop',
 };
