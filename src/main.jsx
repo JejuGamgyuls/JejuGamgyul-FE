4a8c9a2ca1e868a1909f9703cd59db8ebdebd2f2
hyorish03 <hyorish2001@gmail.com>
2024-09-24 14:27:34 +0900

    Feat: MSW 초기 세팅 및 getRouteInfo API 구현

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import './index.css';
-
 import App from './App.jsx';
 import { GlobalStyle } from '../globalStyle';
+import { worker } from '../mocks/browser';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <RecoilRoot>
-        <GlobalStyle />
-        <App />
-      </RecoilRoot>
-    </StrictMode>
-  </BrowserRouter>,
-);
+// 비동기 함수로 정의
+async function startWorkerAndRender() {
+  await worker.start(); // worker를 비동기적으로 시작
+  createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <StrictMode>
+        <RecoilRoot>
+          <GlobalStyle />
+          <App />
+        </RecoilRoot>
+      </StrictMode>
+    </BrowserRouter>,
+  );
+}
+
+// 비동기 함수 호출
+startWorkerAndRender();
--- src/pages/mainpage/index.jsx
+++ src/pages/mainpage/index.jsx
@@ -1,15 +1,21 @@
 import NavigationBar from '@components/NavigationBar';
 import SideBar from '@components/SideBar';
+import axios from 'axios';
 import { NavermapsProvider } from 'react-naver-maps';
 
 import NaverMapContainer from './NaverMapContainer';
 import * as S from './styles';
 function Mainpage() {
   const naverMapClientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
-
+  const apitest = async () => {
+    const response = await axios.get('/getRouteInfo', {});
+    const data = response;
+    console.log(data);
+  };
   return (
     <>
       <S.SideWrapper>
+        <button onClick={apitest}>api test</button>
         <NavigationBar />
         <SideBar />
       </S.SideWrapper>
