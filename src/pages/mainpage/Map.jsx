84635de45e09ecd49b99e1a2404a65d822253695
hyorish03 <hyorish2001@gmail.com>
2024-09-24 18:31:58 +0900

    JG-30 Feat: 중앙차로 버스정류장에 대한 마커 구현

--- src/pages/mainpage/Map.jsx
+++ src/pages/mainpage/Map.jsx
@@ -1,7 +1,7 @@
+import filtered_stops from '@assets/busLocationInfo/filtered_stops.json';
 import { geolocationOptions } from '@constants/const';
 import useCurrentLocation from '@hooks/useCurrentLocation.js';
 import { Marker, NaverMap, useNavermaps } from 'react-naver-maps';
-
 function Map() {
   const navermaps = useNavermaps();
   const { location: currentLocation, error: currentError } = useCurrentLocation(geolocationOptions);
@@ -17,6 +17,13 @@ function Map() {
   return (
     <NaverMap defaultCenter={new navermaps.LatLng(latitude, longitude)} defaultZoom={16}>
       <Marker position={new navermaps.LatLng(latitude, longitude)} />
+      {filtered_stops.map((stop) => (
+        <Marker
+          key={stop.stops_no}
+          position={{ lat: parseFloat(stop.ycrd), lng: parseFloat(stop.xcrd) }}
+          title={stop.stops_nm}
+        />
+      ))}
     </NaverMap>
   );
 }
