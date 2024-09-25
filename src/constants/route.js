ec70e4da50f496ea646c36e9c8dd06535ac4276b
hyorish03 <hyorish2001@gmail.com>
2024-09-25 13:52:47 +0900

    JG-32 Chore: ROUTE 상수 설명 주석 추가

--- src/constants/route.js
+++ src/constants/route.js
@@ -3,6 +3,6 @@ export const ROUTE = {
   SIGNUP: '/signup',
   LOGIN: '/login',
   FAVORITE: '/favorites',
-  BUSFIND: '/search/bus/:busNumber',
-  BUSSTOP: '/search/station/:busStop',
+  BUSFIND: '/search/bus/:busNumber', // 버스 번호로 검색
+  BUSSTOP: '/search/station/:busStop', // 버스정류장 검색
 };
