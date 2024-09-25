bc3b992540212c0b7bbc41eb8e382b1b4823fe72
hyorish03 <hyorish2001@gmail.com>
2024-09-25 10:58:37 +0900

    Feat: useGetDirection 함수 메모이제이션 적용

--- src/hooks/useGetDirection.js
+++ src/hooks/useGetDirection.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const useGetDirection = (busRouteId) => {
   const [direction, setDirection] = useState({ from: '', to: '' });
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const getDirection = async () => {
-      try {
-        const res = await axios.get('http://localhost:8080/getRouteInfo', {
-          params: {
-            busRouteId: busRouteId,
-          },
-        });
+  const getDirection = useCallback(async () => {
+    try {
+      const res = await axios.get('http://localhost:8080/getRouteInfo', {
+        params: {
+          busRouteId: busRouteId,
+        },
+      });
 
-        setDirection({
-          from: res.data.msgBody.itemList[0].stStationNm,
-          to: res.data.msgBody.itemList[0].edStationNm,
-        });
-      } catch (e) {
-        setError(e);
-      }
-    };
-    getDirection();
+      setDirection({
+        from: res.data.msgBody.itemList[0].stStationNm,
+        to: res.data.msgBody.itemList[0].edStationNm,
+      });
+    } catch (e) {
+      setError(e);
+    }
   }, [busRouteId]);
+
+  useEffect(() => {
+    getDirection();
+  }, [getDirection]);
 
   return { direction, error };
 };
