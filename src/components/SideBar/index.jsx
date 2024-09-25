25e2279e2be644f4da15e9e1f37f2c79cfb592bc
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-25 10:31:40 +0900

    JG-23 Feat: 종점 방향 선택 시 SideBar 내에서 상단으로 이동

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,10 +1,11 @@
-import { navigationBarState } from '@atoms/navigationBarState';
+import { navigationBarState, scrollByDirectionState } from '@atoms/NavigationBarState';
 import { CATEGORY } from '@constants/const';
 import BusDetailInfo from '@pages/busFindpage/components';
 import BusStopInfo from '@pages/busStopFindPage/components';
 import Favorites from '@pages/FavoritesPage/components';
 import BusStopsAround from '@pages/mainpage/components';
-import { useRecoilState } from 'recoil';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import FindBusInput from './components/FindBusInput';
 import * as S from './styles';
@@ -20,10 +21,47 @@ const SIDE_BAR_MAP = {
 
 function SideBar() {
   const [category] = useRecoilState(navigationBarState);
+  const [, setScrollPosition] = useState(0);
+  const selectedDirection = useRecoilValue(scrollByDirectionState);
+  const scrollRef = useRef();
+
+  const setScrollToPosition = (position) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({
+        top: position,
+        behavior: 'smooth',
+      });
+    }
+  };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (scrollRef.current) {
+        setScrollPosition(scrollRef.current.scrollTop);
+      }
+    };
+
+    const currentRef = scrollRef.current;
+
+    if (currentRef) {
+      currentRef.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (currentRef) {
+        currentRef.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+  useEffect(() => {
+    if (selectedDirection == 'end') {
+      setScrollToPosition(292);
+    }
+  }, [selectedDirection]);
+
   return (
     <S.Wrapper>
       {category !== CATEGORY.FAVORITE && <FindBusInput />}
-      <S.BusStopItemWrapper>{SIDE_BAR_MAP[category]()}</S.BusStopItemWrapper>
+      <S.BusStopItemWrapper ref={scrollRef}>{SIDE_BAR_MAP[category]()}</S.BusStopItemWrapper>
     </S.Wrapper>
   );
 }
