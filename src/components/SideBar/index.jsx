c379e9bdfd13c56f58232c6d975ec9a154913472
hyorish03 <hyorish2001@gmail.com>
2024-09-25 11:23:57 +0900

    JG-32 Fix: 사이드바 컴포넌트 렌더링 방식을 함수로 변경
    - [CATEGORY.HOME]의 JSX 표현식을 함수로 수정하여 렌더링 방식을 개선
      - 기존: <BusStopsAround />
      - 변경: () => <BusStopsAround />

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -10,22 +10,26 @@ import FindBusInput from './components/FindBusInput';
 import * as S from './styles';
 
 const SIDE_BAR_MAP = {
-  [CATEGORY.HOME]: BusStopsAround,
-  [CATEGORY.FAVORITE]: Favorites,
+  [CATEGORY.HOME]: () => <BusStopsAround />,
+  [CATEGORY.FAVORITE]: () => <Favorites />,
   [CATEGORY.TIMETABLE]: () => <div>시간표</div>,
   [CATEGORY.MYINFO]: () => <div>내 정보</div>,
-  [CATEGORY.BUSDETAILINFO]: BusDetailInfo,
-  [CATEGORY.BUSSTOPINFO]: BusStopInfo,
+  [CATEGORY.BUSDETAILINFO]: () => <BusDetailInfo />,
+  [CATEGORY.BUSSTOPINFO]: () => <BusStopInfo />,
 };
 
 function SideBar() {
   const [category] = useRecoilState(navigationBarState);
+
+  const Component = SIDE_BAR_MAP[category];
+
   return (
     <S.Wrapper>
       {category !== CATEGORY.FAVORITE && <FindBusInput />}
-      <S.BusStopItemWrapper>{SIDE_BAR_MAP[category]()}</S.BusStopItemWrapper>
+      <S.BusStopItemWrapper>
+        {Component ? <Component /> : <div>카테고리 없음</div>}
+      </S.BusStopItemWrapper>
     </S.Wrapper>
   );
 }
-
 export default SideBar;
