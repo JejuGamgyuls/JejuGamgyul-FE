f7842487047e44d60c744e4f7ea63025803cbd03
hyorish03 <hyorish2001@gmail.com>
2024-09-26 13:49:32 +0900

    Feat: 버스 정류장 검색 결과 없을 경우 처리

--- src/pages/busStopFindPage/components/index.jsx
+++ src/pages/busStopFindPage/components/index.jsx
@@ -1,6 +1,7 @@
 import filtered_stops from '@assets/busLocationInfo/filtered_stops.json';
 import BusStopItem from '@pages/mainpage/components/BusStopItem';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 function BusStopFind() {
   const { busStopName } = useParams();
   const busStopFound = filtered_stops.filter((busStop) => {
@@ -8,18 +9,36 @@ function BusStopFind() {
   });
 
   return (
-    <div>
-      {busStopFound.map((busStop, index) => (
-        <BusStopItem
-          key={index}
-          busStopName={busStop.stopsNm}
-          busStopNumber={busStop.stopsNo}
-          busDirection={busStop.direction}
-          busStopId={busStop.nodeId}
-        />
-      ))}
-    </div>
+    <Wrapper>
+      {busStopFound.length !== 0 ? (
+        busStopFound.map((busStop, index) => (
+          <BusStopItem
+            key={index}
+            busStopName={busStop.stopsNm}
+            busStopNumber={busStop.stopsNo}
+            busDirection={busStop.direction}
+            busStopId={busStop.nodeId}
+          />
+        ))
+      ) : (
+        <NoResult>검색 결과가 없습니다. {`:(`}</NoResult>
+      )}
+    </Wrapper>
   );
 }
 
 export default BusStopFind;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const NoResult = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60%;
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
