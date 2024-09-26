5315ec086d4fa42231558da60cde04994b9fd0f6
hyorish03 <hyorish2001@gmail.com>
2024-09-26 16:02:21 +0900

    Feat: 로그인 기능 구현

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,33 @@
 import SubmitButton from '@components/Buttons/SubmitButton';
 import Input from '@components/Input';
+import axios from 'axios';
+import { useRef } from 'react';
 
 import * as S from './styles';
 
 function LoginPage() {
+  const idRef = useRef();
+  const pwdRef = useRef();
+
+  const handleLogin = async () => {
+    const id = idRef.current.value;
+    const pwd = pwdRef.current.value;
+    console.log(id, pwd);
+    try {
+      const res = await axios.post('http://localhost:8080/login', {
+        userId: id,
+        pwd,
+      });
+
+      if (res.status === 202) {
+        alert('로그인 성공');
+        window.location.href = '/';
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
   return (
     <S.Wrapper>
       <S.Header>
@@ -11,11 +35,11 @@ function LoginPage() {
       </S.Header>
       <S.Body>
         <S.InputsWrapper>
-          <Input placeholder="아이디를 입력해주세요" />
-          <Input type="password" placeholder="비밀번호를 입력해주세요" />
+          <Input ref={idRef} placeholder="아이디를 입력해주세요" />
+          <Input ref={pwdRef} type="password" placeholder="비밀번호를 입력해주세요" />
         </S.InputsWrapper>
         <S.ButtonsWrapper>
-          <SubmitButton text="로그인" />
+          <SubmitButton text="로그인" handleClick={handleLogin} />
           <SubmitButton
             text="회원가입"
             background="#fff"
