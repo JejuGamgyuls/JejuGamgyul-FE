65d841c0f78dd0c1d132755fbdef4a75cd40b6c5
hyorish03 <hyorish2001@gmail.com>
2024-09-27 15:00:01 +0900

    JG-39 Chore: 주석 코드 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,7 @@ import { RecoilRoot } from 'recoil';
 import './index.css';
 import App from './App.jsx';
 import { GlobalStyle } from '../globalStyle';
-import { worker } from '../mocks/browser';
 
-// 비동기 함수로 정의
-// async function startWorkerAndRender() {
-//   await worker.start(); // worker를 비동기적으로 시작
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
@@ -21,7 +17,3 @@ createRoot(document.getElementById('root')).render(
     </StrictMode>
   </BrowserRouter>,
 );
-// }
-
-// // 비동기 함수 호출
-// startWorkerAndRender();
