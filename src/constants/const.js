da78c49a7e93df7a721f0e87396a98b4e62f10e9
hyorish03 <hyorish2001@gmail.com>
2024-09-28 23:40:51 +0900

    JG-40 Feat: 버스 태그, 버스 타입 관련 상수 추가

--- src/constants/const.js
+++ src/constants/const.js
@@ -19,3 +19,29 @@ export const STYLE = {
   CURRENT_LOCATION_HEADER_HEIGHT: 60,
   FIND_BUS_INPUT_HEIGHT: 45,
 };
+
+export const ROUTETYPETAG = {
+  1: '공항',
+  2: '마을',
+  3: '간선',
+  4: '지선',
+  5: '순환',
+  6: '광역',
+  7: '인천',
+  8: '경기',
+  9: '폐지',
+  0: '공용',
+};
+
+export const ROUTETYPECOLORS = {
+  1: '#000',
+  2: '#000',
+  3: '#4086F4',
+  4: '#4086F4',
+  5: '#FBC02D',
+  6: '#F44336',
+  7: '#386de8',
+  8: '#000',
+  9: '#000',
+  0: '#000',
+};
