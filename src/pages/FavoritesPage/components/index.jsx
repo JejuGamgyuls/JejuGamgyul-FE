14e72cab9c848cde58da8332d47188ed31e0c54b
hyorish03 <hyorish2001@gmail.com>
2024-09-29 00:47:30 +0900

    JG-40 Delete: 즐겨찾기 페이지 내 typeDef 삭제

--- src/pages/FavoritesPage/components/index.jsx
+++ src/pages/FavoritesPage/components/index.jsx
@@ -1,98 +1,16 @@
 import * as busApi from '@pages/busStopPage/api';
-import BusInfoItem from '@pages/busStopPage/components/BusInfoItem';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import FavoriteItem from './FavoriteItem';
 import Header from './Header';
 
-/**
- * @typedef {Object} busInfoList[]
- * @property {string} arsId
- * @property {string} busRouteId
- * @property {string} busRouteNm
- * @property {string} busType1
- * @property {string} busType2
- * @property {string} exps1
- * @property {string} exps2
- * @property {string} full1
- * @property {string} full2
- * @property {string} isArrive1
- * @property {string} isArrive2
- * @property {string} isLast1
- * @property {string} isLast2
- * @property {string} plainNo1
- * @property {string} plainNo2
- * @property {string} rtNm
- * @property {string} sectOrd1
- * @property {string} sectOrd2
- * @property {string} stId
- * @property {string} stNm
- * @property {string} staOrd
- * @property {string} stationNm1
- * @property {string} stationNm2
- * @property {string} term
- * @property {string} traSpd1
- * @property {string} traSpd2
- * @property {string} traTime1
- * @property {string} traTime2
- * @property {string} vehId1
- * @property {string} vehId2
- */
-/**
- * @type {busInfoList[]}
- */
-
 function Favorites() {
   const [isLoading, setIsLoading] = useState(true);
-
-  /**
-   * @typedef {Object} busInfoList[]
-   * @property {string} arsId
-   * @property {string} busRouteId
-   * @property {string} busRouteNm
-   * @property {string} busType1
-   * @property {string} busType2
-   * @property {string} exps1
-   * @property {string} exps2
-   * @property {string} full1
-   * @property {string} full2
-   * @property {string} isArrive1
-   * @property {string} isArrive2
-   * @property {string} isLast1
-   * @property {string} isLast2
-   * @property {string} plainNo1
-   * @property {string} plainNo2
-   * @property {string} rtNm
-   * @property {string} sectOrd1
-   * @property {string} sectOrd2
-   * @property {string} stId
-   * @property {string} stNm
-   * @property {string} staOrd
-   * @property {string} stationNm1
-   * @property {string} stationNm2
-   * @property {string} term
-   * @property {string} traSpd1
-   * @property {string} traSpd2
-   * @property {string} traTime1
-   * @property {string} traTime2
-   * @property {string} vehId1
-   * @property {string} vehId2
-   */
-  /**
-   * @type {busInfoList[]}
-   */
+  const [reloadTime, setReloadTime] = useState(new Date());
 
   const [busInfoList, setBusInfoList] = useState([]);
 
-  /**
-   * @typedef {Object} busData[]
-   * @property {string} busStopId
-   * @property {string} busStopName
-   * @property {string} routeId
-   * @property {string} routeName
-   * @property {string} userId
-   */
   const refreshFavoritBusInfo = async () => {
     try {
       setBusInfoList([]);
@@ -110,6 +28,15 @@ function Favorites() {
       throw new Error(e);
     }
   };
+  useEffect(() => {
+    refreshFavoritBusInfo();
+
+    const intervalId = setInterval(() => {
+      setReloadTime(new Date());
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   useEffect(() => {
     refreshFavoritBusInfo();
@@ -117,7 +44,7 @@ function Favorites() {
 
   return (
     <Wrapper>
-      <Header />
+      <Header reloadTime={reloadTime} />
       <BodyWrapper>
         {isLoading ? (
           <div>로딩중</div>
