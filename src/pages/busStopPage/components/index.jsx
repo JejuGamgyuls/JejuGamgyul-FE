4327a99de0f116c1b2e2a0cb8a40bd6bbd8f9031
rhdmswls12 <rhgusdnr1293@naver.com>
2024-09-29 23:58:00 +0900

    JG-38 Fix: URL 변경 감지 및 새로운 busStopId에 맞는 버스 정보 가져오기 로직 추가

--- src/pages/busStopPage/components/index.jsx
+++ src/pages/busStopPage/components/index.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import BusInfoItem from './BusInfoItem';
 import BusStopHeader from './BusStopHeader';
 import { getLowArrInfoByStId } from '../api';
 
-function BusStopInfo({ busStopId }) {
+function BusStopInfo() {
+  const location = useLocation();
   const [busInfoList, setBusInfoList] = useState([]);
   const [reloadTime, setReloadTime] = useState(new Date());
+
+  const queryParams = new URLSearchParams(location.search);
+  const busStopId = queryParams.get('busStopId');
+
   const fetchBusInfo = async () => {
+    if (!busStopId) return;
     try {
       const busData = await getLowArrInfoByStId(busStopId); // 예시 stId 사용
       setBusInfoList(busData);
@@ -21,10 +28,11 @@ function BusStopInfo({ busStopId }) {
 
     const intervalId = setInterval(() => {
       setReloadTime(new Date());
+      fetchBusInfo();
     }, 5000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [busStopId]);
 
   return (
     <div>
